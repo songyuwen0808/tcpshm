74ac1be73bcd1c5f43355c070e1b9eb1677d051b
songyuwen <songyuwen@baidu.com>
2020-09-03 10:46:00 +0800

    modify clear.sh

--- test/build.sh
+++ test/build.sh
@@ -1,2 +1,2 @@
-g++ -std=c++11 -O3 -o echo_server echo_server.cc -lrt -lpthread
-g++ -std=c++11 -O3 -o echo_client echo_client.cc -lrt -lpthread
+g++ -std=c++11 -Ofast -o echo_server echo_server.cc -lrt -lpthread
+g++ -std=c++11 -Ofast -o echo_client echo_client.cc -lrt -lpthread
--- test/clear.sh
+++ test/clear.sh
@@ -5,3 +5,5 @@ rm -rf c2
 rm -rf c3
 rm -rf server
 rm -f /dev/shm/*
+rm -f echo_server
+rm -f echo_client
